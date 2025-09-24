a4ca1586812e1b1d875821f804a3ffb79460ea4b
anu-anas-21 <anasanu0174@gmail.com>
2025-09-24 12:46:05 +0530

    Initial commit

---
+++ VaniteMonde/src/main/java/com/vanitemonde/inventorymng/VaniteMondeApplication.java
@@ -0,0 +1,13 @@
+package com.vanitemonde.inventorymng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaniteMondeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaniteMondeApplication.class, args);
+	}
+
+}
